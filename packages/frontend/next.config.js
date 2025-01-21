567be5d0ed70fb150fb01d0e07a712ec0e800a88
Sero <69639595+Seroxdesign@users.noreply.github.com>
2025-01-21 07:10:45 -0500

    isr

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
-  // Since we're fetching from a public URL, we don't need to configure additional domains
 }
 
 module.exports = nextConfig 
